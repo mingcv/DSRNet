04c2b2de7d01cd2d3fa8e7a17c7dc7199581794b
Qiming Hu <18002177060@163.com>
2023-08-29 18:26:26 +0800

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -3,4 +3,4 @@ python train_sirs.py --inet dsrnet --model dsrnet_model_sirs --dataset sirs_data
 
 python eval_sirs.py --inet dsrnet_s --model dsrnet_model_sirs --dataset sirs_dataset  --name dsrnet_s_test --hyper --if_align --resume --weight_path "./weights/dsrnet_s_epoch14.pt" --base_dir "[YOUR_DATA_DIR]"
 
-python eval_sirs.py --inet dsrnet_l --model dsrnet_model_sirs --dataset sirs_dataset  --name dsrnet_l_test --hyper --if_align --resume --weight_path "/home/limj/huqiming/workspace/DSRNet/checkpoints/dsrnet_l_shared_b/weights/dsrnet_l_epoch18.pt" --base_dir "/home/huqiming/datasets/sirs"
+python eval_sirs.py --inet dsrnet_l --model dsrnet_model_sirs --dataset sirs_dataset  --name dsrnet_l_test --hyper --if_align --resume --weight_path "./weights/dsrnet_l_epoch18.pt" --base_dir "[YOUR_DATA_DIR]"
