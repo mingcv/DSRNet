8002ed52071add9feb877a6642b9e7f336a0df91
Qiming Hu <18002177060@163.com>
2023-08-29 18:26:49 +0800

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,5 +1,5 @@
 ### To train the model
-python train_sirs.py --inet dsrnet --model dsrnet_model_sirs --dataset sirs_dataset --loss losses  --name dsrnet  --lambda_vgg 0.01 --lambda_rec 0.2 --if_align --seed 2018 --base_dir "[YOUR_DATA_DIR]"
+python train_sirs.py --inet dsrnet_l --model dsrnet_model_sirs --dataset sirs_dataset --loss losses  --name dsrnet_l  --lambda_vgg 0.01 --lambda_rec 0.2 --if_align --seed 2018 --base_dir "[YOUR_DATA_DIR]"
 
 python eval_sirs.py --inet dsrnet_s --model dsrnet_model_sirs --dataset sirs_dataset  --name dsrnet_s_test --hyper --if_align --resume --weight_path "./weights/dsrnet_s_epoch14.pt" --base_dir "[YOUR_DATA_DIR]"
 
